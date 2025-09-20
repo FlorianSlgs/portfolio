6f1391f69783a7578b5cf24bcf313abcd1b56197
Florian Salengros <florian.salengros@gmail.com>
2025-09-20 16:16:01 +0200

    footer et typed

--- src/app/components/DOM/section1/section1.ts
+++ src/app/components/DOM/section1/section1.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { ButtonComponent } from '../../shared/button/button';
+
+// Pas d'import statique de Typed.js pour éviter le chargement SSR
 
 @Component({
   selector: 'app-section1',
@@ -7,7 +10,12 @@ import { ButtonComponent } from '../../shared/button/button';
   templateUrl: './section1.html',
   styleUrl: './section1.css'
 })
-export class Section1 {
+export class Section1 implements AfterViewInit {
+  
+  @ViewChild('typedElement', { static: true }) typedElement!: ElementRef;
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   handleButtonClick() {
     console.log('Bouton Contact cliqué !');
   }
@@ -34,4 +42,28 @@ export class Section1 {
       class: 'w-30 h-30 object-contain',
     }
   ];
+
+  ngAfterViewInit() {
+    // S'assurer que l'animation ne se lance QUE côté client
+    if (isPlatformBrowser(this.platformId)) {
+      // Import dynamique pour éviter le chargement côté serveur
+      import('typed.js').then((TypedModule) => {
+        const Typed = TypedModule.default;
+        
+        const options = {
+          strings: ['Angular', 'TypeScript', 'SCSS', 'Animations', 'Open Source'],
+          typeSpeed: 80,
+          backSpeed: 40,
+          backDelay: 1000,
+          startDelay: 500,
+          loop: true,
+          showCursor: true,
+          cursorChar: '|',
+          smartBackspace: true
+        };
+
+        new Typed(this.typedElement.nativeElement, options);
+      });
+    }
+  }
 }
