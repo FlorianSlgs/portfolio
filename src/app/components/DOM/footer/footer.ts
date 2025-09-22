265ac0b9d2a55ecd456901342775fc5f395187b0
Florian Salengros <florian.salengros@gmail.com>
2025-09-22 06:44:50 +0200

    plus que les textes a faire

--- src/app/components/DOM/footer/footer.ts
+++ src/app/components/DOM/footer/footer.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
+  isModalOpen = signal(false);
 
+  openModal() {
+    this.isModalOpen.set(true);
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
+  }
+
+  // Fermer le modal en cliquant en dehors
+  onBackdropClick(event: Event) {
+    if (event.target === event.currentTarget) {
+      this.closeModal();
+    }
+  }
 }
