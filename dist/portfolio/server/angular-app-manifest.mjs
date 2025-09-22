8d4d628f21e663e46d59c28caa489d2991ccab1b
Florian Salengros <florian.salengros@gmail.com>
2025-09-22 10:04:29 +0200

    build

--- dist/portfolio/server/angular-app-manifest.mjs
+++ dist/portfolio/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 32605, hash: '399b6cb6db859bea668b54d1ac0d4cb088888ffa7977a683115cac21552eeab1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 25129, hash: '03430ac3d8651234012e9ebc5ed192f552e070ed0a0ac33ada3d30d2e13f9983', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 107348, hash: '275cd3bf19814132a1f1d87737db7fdb633293ee54835b41345d5da877b72cd7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-SEATB56S.css': {size: 46726, hash: 'C33ctmaM3wc', text: () => import('./assets-chunks/styles-SEATB56S_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 33629, hash: '832d77d157d6fe2c1c4d8ed6054a0890f1049ba1f03148f5aa08416fe8a8984a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 25129, hash: '446a985d6f06641bbb7e49a13da72023d95732c3c5e45e951af563818841be87', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 108426, hash: 'c08d00620cbd5da045090a540f7d0375bf65337cb91c22d70985b2194185a343', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-IJGJW56C.css': {size: 49501, hash: 'Wt/n5ftMpVw', text: () => import('./assets-chunks/styles-IJGJW56C_css.mjs').then(m => m.default)}
   },
 };
